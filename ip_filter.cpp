2adb6baf2a87163a2b76a9bfbd2edc637f528f2f
SergeiNA <nikseas@yandex.ru>
2019-10-21 01:42:58 +0300

    refactoring main func

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -14,27 +14,10 @@ int main(int argc, char const *argv[])
 			return move(ip_adrr_v);
 		}();
 
-		auto ip_adrr_v_1 = filter(ip_adrr_v, 1);
-		auto ip_adrr_v_46_70 = filter(ip_adrr_v, 46, 70);
-		auto ip_adrr_v_any_46 = filter_any(ip_adrr_v, 46);
-
-		ip_adrr_v.reserve(ip_adrr_v.size() + ip_adrr_v_1.size() 
-			+ ip_adrr_v_46_70.size() + ip_adrr_v_any_46.size());
-
-		ip_adrr_v.insert(end(ip_adrr_v),
-			std::make_move_iterator(begin(ip_adrr_v_1)),
-			std::make_move_iterator(end(ip_adrr_v_1)));
-
-
-		ip_adrr_v.insert(end(ip_adrr_v),
-			std::make_move_iterator(begin(ip_adrr_v_46_70)),
-			std::make_move_iterator(end(ip_adrr_v_46_70)));
-
-		ip_adrr_v.insert(end(ip_adrr_v),
-			std::make_move_iterator(begin(ip_adrr_v_any_46)),
-			std::make_move_iterator(end(ip_adrr_v_any_46)));
-
 		output(ip_adrr_v);
+		output(filter(ip_adrr_v, 1));
+		output(filter(ip_adrr_v, 46, 70));
+		output(filter_any(ip_adrr_v, 46));
 
     }
     catch(const std::exception &e)
